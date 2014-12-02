d32ac1cf5e4cd96e2fb896010d4fb3a2d9e78de1
Robin Verlangen <robin@us2.nl>
2014-12-02 15:44:53 +0100

    Debug

--- client.go
+++ client.go
@@ -1,8 +1,24 @@
 package veritas
 
+import (
+	"fmt"
+	"log"
+)
+
 type VeritasClient struct {
+	customerId    int
+	applicationId int
+	secureToken   string
 }
 
-func NewClient() *VeritasClient {
-	return &VeritasClient{}
+func NewClient(customerId int, applicationId int, secureToken string) *VeritasClient {
+	return &VeritasClient{
+		customerId:    customerId,
+		applicationId: applicationId,
+		secureToken:   secureToken,
+	}
+}
+
+func (v *VeritasClient) PrintDebug() {
+	log.Println(fmt.Sprintf("Veritas client (customer: %d) (app: %d)", v.customerId, v.applicationId))
 }
